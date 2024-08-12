aa2a636806689b1982acd5741480f523463e10ef
Alfred Augustine <77981488+AsbestosLampshade@users.noreply.github.com>
2024-08-12 22:16:36 +0530

    Update BMP180.ino

--- BMP180.ino
+++ BMP180.ino
@@ -6,17 +6,15 @@ void setup() {
   Serial.begin(57600);
   BMP_t BMP;
   Serial.println("Code running");
-  Serial.println(BMP.calculateTemperature());
-  //word temp=0xABCD;
-  //BEtoLEConvert(temp);
-  //Serial.println();
-  //Serial.println(temp,HEX);
-  Serial.println(BMP.calculatePressure());
+  Serial.print("The temperature is:");
+  Serial.print(BMP.getTemperature());
+  Serial.println();
+  Serial.print("The Pressure is:");
+  Serial.print(BMP.getPressure());
+  Serial.println();
 }
 
 
 void loop() {
   // put your main code here, to run repeatedly:
-     
-  
 }
